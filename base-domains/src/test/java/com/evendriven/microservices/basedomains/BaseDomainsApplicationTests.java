7bf5e1986fa66010e1c999eb156bce5bb6c3f356
Max Dev <shoebmoh13@gmail.com>
2023-07-01 15:26:55 +0530

    adding event-driven microservices

---
+++ base-domains/src/test/java/com/evendriven/microservices/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evendriven.microservices.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order-service/src/main/java/com/evendriven/microservices/orderservice/config/KafkaTopicConfig.java
@@ -0,0 +1,25 @@
+package com.evendriven.microservices.orderservice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaTopicConfig.class);
+
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    @Bean
+    public NewTopic topic(){
+        LOGGER.info("created new topic {}",topicName);
+        return TopicBuilder.name(topicName).build();
+    }
+
+}
